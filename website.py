e6fea14ee14f42646444eb70a9217dff6cf95080
Alber Analafean <105194751+alberanalafean22@users.noreply.github.com>
2024-11-16 02:25:32 +0700

    Update website.py

--- website.py
+++ website.py
@@ -107,7 +107,8 @@ html_code = """
     
     <div class="footer">
         <h2><stronng>Kelompok 13 Deep Learning</strong></h2>
-        <h2><stronng>Program Studi Sains Data, Fakultas Sains, Institut Teknologi Sumatera</strong></h2>
+        <h2><stronng>Program Studi Sains Data, Fakultas Sains</strong></h2> 
+        <h2><strong>Institut Teknologi Sumatera</strong></h2>
         <h3>Anggota Kelompok</h3>
         <h4>Rizki Adrian Bennovry-121450073 | Project Manager</h4>
         <h4>Alber Analafean-121450146 | Research & Development Manager</h4>
