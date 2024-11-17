0b3a47c9dd6650c434cacab34cce7af72213f5f4
Alber Analafean <105194751+alberanalafean22@users.noreply.github.com>
2024-11-17 16:48:01 +0700

    Update website.py

--- website.py
+++ website.py
@@ -116,8 +116,8 @@ html_code = """
         <img src="https://raw.githubusercontent.com/alberanalafean22/DeteksiKlasifikasiSpesiesBurung/main/Logo3.png" alt="Logo 3" width="75" height="75">
         <img class="logo" src="https://raw.githubusercontent.com/alberanalafean22/DeteksiKlasifikasiSpesiesBurung/main/Logo0.png" alt="Logo kelompok" width="75" height="75">
         </div>
-        <h3><strong>Program Studi Sains Data</strong></h3>
-        <h3><strong>Fakultas Sains</strong></h3>
+        <h3><stronng>Kelompok 13 Deep Learning</strong></h3>
+        <h3><strong>Program Studi Sains Data, Fakultas Sains</strong></h3>
         <h3><strong>Institut Teknologi Sumatera</strong></h3>
         <h2><strong>#DATASCIENCE #ITERAFORSUMATERA</strong></h2>
         <h3>ㅤ</h3>
