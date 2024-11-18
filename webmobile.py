f2ab0a7fdd84fe9cf240b26e073fe3aeb9e3bf27
Alber Analafean <105194751+alberanalafean22@users.noreply.github.com>
2024-11-18 16:33:10 +0700

    Update webmobile.py

--- webmobile.py
+++ webmobile.py
@@ -51,7 +51,7 @@ html_code = """
         }
         .upload-form {
             background-color: rgba(255, 255, 255, 0.2);
-            border-radius: 5px;
+            border-radius: 15px;
             padding: 15px;
             box-shadow: 0 4px 40px rgba(0, 0, 0, 0.5);
             display: flex;
