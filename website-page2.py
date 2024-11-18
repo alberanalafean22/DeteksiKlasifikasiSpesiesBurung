c7797ebfdd9750673dc55ff55b7f5a51d930948f
Alber Analafean <105194751+alberanalafean22@users.noreply.github.com>
2024-11-18 16:47:23 +0700

    Create website-page2.py

---
+++ website-page2.py
@@ -0,0 +1,79 @@
+import streamlit as st
+
+def add_bg_from_url():
+    st.markdown(
+         f"""
+         <style>
+         .stApp {{
+             background-image: url("https://images.unsplash.com/photo-1444464666168-49d633b86797?q=80&w=1469&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D");   
+
+             background-size:   
+ cover;
+             background-position: top center;
+         }}
+         </style>
+         """,
+         unsafe_allow_html=True
+     )
+
+add_bg_from_url()
+# HTML code as a multi-line string
+html_code = """
+<!DOCTYPE html>
+<html lang="id">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>deeplearning13</title>
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.3/css/all.min.css">
+    <link href="https://fonts.googleapis.com/css2?family=Roboto:wght@400;700&display=swap" rel="stylesheet">
+    <style>
+        body {
+            background-size: cover;
+            color: white;
+            font-family: 'Roboto', sans-serif;
+            display: flex;
+            flex-direction: column;
+            justify-content: center;
+            align-items: center;
+            height: 100vh;
+            margin: 0;
+            text-align: center;
+        }
+        
+        h1 {
+            background-color: rgba(0, 128, 0, 0.8);
+            padding: 20px;
+            border-radius: 15px;
+            margin-bottom: 20px;
+            box-shadow: 0 4px 20px rgba(0, 0, 0, 0.5);
+        
+        }
+        img {
+            margin-top: 20px;
+            max-width: 100%;
+            border-radius: 15px;
+            box-shadow: 0 4px 20px rgba(0, 0, 0, 0.5);
+        }
+        
+ 
+    </style>
+</head>
+<body>
+    <h1>Deteksi Klasifikasi Suara Kicauan Burung di Taman Nasional Way Kambas berbasis Deep Learning - Metode Transfer Learning (Arsitektur ConvNeXt)</h1>
+        <div> 
+        <img src="https://raw.githubusercontent.com/alberanalafean22/DeteksiKlasifikasiSpesiesBurung/main/asset/Logo1.png" alt="Logo 1" width="100" height="100">    
+        <img src="https://raw.githubusercontent.com/alberanalafean22/DeteksiKlasifikasiSpesiesBurung/main/asset/Logo2.png" alt="Logo 2" width="100" height="100"> 
+        <img src="https://raw.githubusercontent.com/alberanalafean22/DeteksiKlasifikasiSpesiesBurung/main/asset/Logo3.png" alt="Logo 3" width="100" height="100">
+        <img src="https://raw.githubusercontent.com/alberanalafean22/DeteksiKlasifikasiSpesiesBurung/main/asset/Logo0.png" alt="Logo kelompok" width="100" height="100">
+        <img src="https://raw.githubusercontent.com/alberanalafean22/DeteksiKlasifikasiSpesiesBurung/main/asset/LogoTN.png" alt="Logo kelompok v" width="100" height="100">
+        </div>
+        <h4>© Developer: Kelompok 13 Deep Learning PAGE 2</h4>
+    </div>
+        
+</body>
+</html>
+"""
+
+# Display the HTML in Streamlit
+st.components.v1.html(html_code, height=700, scrolling=True)
