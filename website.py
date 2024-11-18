b6ff2275fc7dce2d112459c7f1aceb126feb4bc2
Alber Analafean <105194751+alberanalafean22@users.noreply.github.com>
2024-11-18 16:35:55 +0700

    Update website.py

--- website.py
+++ website.py
@@ -116,5 +116,5 @@ html_code = """
 """
 
 # Display the HTML in Streamlit
-st.components.v1.html(html_code, height=700,  scrolling=True)
+st.components.v1.html(html_code, height=850,  scrolling=True)
 
